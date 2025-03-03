dc274cf991cbc06f55ebdbc4543c0b150cc29500
Nikhil Munda <bt23cse092@iiitn.ac.in>
2025-03-03 14:29:05 +0530

    damn

---
+++ conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = "nigger"
+/*
+// const {score}= req.body
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumer = Number(score)
+
+console.log(typeof valueInNumer)
+console.log(valueInNumer)
+//NaN is not a number its type is number
+*/
+
+// let isLoggedIn = "nikhil"
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+// console.log(typeof (booleanisLoggedIn))
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+
+let str1 = "hello"
+let str2 = " nikhil"
+let str3 = str1+str2
+console.log(str3)
